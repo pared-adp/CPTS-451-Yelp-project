2196a1cf3322e5f2c64b1a1f4c6d784bd5a57107
Angelina Paredes <33231863+pared-adp@users.noreply.github.com>
2019-02-07 00:14:55 -0800

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  6 23:59:23 2019
+
+@author: Angelina
+"""
+
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QTableWidget,QTableWidgetItem,QVBoxLayout
+from PyQt5 import uic, QtCore
+from PyQt5.QtGui import QIcon, QPixmap
+
+qtCreatorFile = "milestoneUI.ui" # Enter file here.
+
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+class milestone1(QMainWindow):
+    def __init__(self):
+        super(milestone1, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = milestone1()
+    window.show()
+    sys.exit(app.exec_())
