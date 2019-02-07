dd030b6f60d8916826a253b773da1695f78c02db
Angelina Paredes <33231863+pared-adp@users.noreply.github.com>
2019-02-07 01:04:51 -0800

    Add files via upload

--- untitled0.py
+++ untitled0.py
@@ -9,6 +9,7 @@ import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QTableWidget,QTableWidgetItem,QVBoxLayout
 from PyQt5 import uic, QtCore
 from PyQt5.QtGui import QIcon, QPixmap
+import psycopg2
 
 qtCreatorFile = "milestoneUI.ui" # Enter file here.
 
@@ -19,8 +20,50 @@ class milestone1(QMainWindow):
         super(milestone1, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-
-
+        self.loadStateList()
+        self.ui.stateList.currentTextChanged.connect(self.stateChanged)
+    def executeQuery(self, sql_str):
+        try: 
+            conn = psycopg2.connect("dbname='milestone1' user ='postgres' host='localhost' password='Mich@el8114'")
+        except:
+            print("Unable to connect")
+        cur = conn.cursor()
+        cur.execute(sql_str)
+        result = cur.fetchall()
+        conn.close()
+        return result
+        
+    def loadStateList(self):
+        self.ui.stateList.clear()
+        sql_str = "SELECT distinct state FROM business ORDER BY state"
+        try:
+            results = self.executeQuery(sql_str)
+            for row in results:
+                self.ui.stateList.addItem(row[0])
+        except:
+            print("Query Failed")
+        self.ui.stateList.setCurrentIndex(-1)
+        self.ui.stateList.clearEditText()
+    
+    def stateChanged(self):
+        self.ui.cityList.clear()
+        if (self.ui.stateList.currentIndex()>=0):
+            state = self.ui.stateList.currentText()
+            sql_str = "SELECT distinct city FROM business WHERE state ='" + state + "' ORDER BY city;" 
+            try:
+                results = self.executeQuery(sql_str)
+                for row in results:
+                    self.ui.cityList.addItem(row[0])
+            except:
+                print("Query Failed!")
+            sql_str = "SELECT name, city, state FROM business WHERE state ='" + state + "' ORDER BY name;"
+            try:
+                results = self.executeQuery(sql_str)
+                for row in results:
+                    
+                
+            except:
+                print("Query Failed!!")
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = milestone1()
