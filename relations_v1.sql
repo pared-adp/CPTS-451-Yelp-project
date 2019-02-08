f548112ecf01c9e95b23525c366fd9fc192f06c6
bbhorace2004 <47283763+bbhorace2004@users.noreply.github.com>
2019-02-07 19:39:04 -0800

    Update relations_v1.sql

--- relations_v1.sql
+++ relations_v1.sql
@@ -2,8 +2,8 @@ CREATE TABLE User (
 	user_ID CHAR,
 	user_name VARCHAR(50),
 	user_stars DECIMAL(3,2),
-  user_yelp_since DATE,
-  user_num_of_fans INTEGAR,
+  	user_yelp_since DATE,
+  	user_num_of_fans INTEGAR,
 	PRIMARY KEY (user_ID) 
 );
 
