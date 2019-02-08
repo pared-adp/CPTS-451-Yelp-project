a6ab9df7e662c1b84aed59b6eb49ab1bd0b42b9a
bbhorace2004 <47283763+bbhorace2004@users.noreply.github.com>
2019-02-07 19:50:22 -0800

    Update relations_v1.sql

--- relations_v1.sql
+++ relations_v1.sql
@@ -1,3 +1,9 @@
+CREATE TABLE Friend (
+	friend_name VARCHAR(50),
+	friend_stars DECIMAL(3,2),
+	friend_yelp_since DATE,
+	PRIMARY KEY (friend_name)
+
 CREATE TABLE User (
 	user_ID CHAR,
 	user_name VARCHAR(50),
