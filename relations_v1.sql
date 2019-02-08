aa39a89667956d58f04fa39de2c3a837d5f2642f
bbhorace2004 <47283763+bbhorace2004@users.noreply.github.com>
2019-02-07 20:24:00 -0800

    Update relations_v1.sql

--- relations_v1.sql
+++ relations_v1.sql
@@ -1,17 +1,28 @@
+CREATE TABLE User (
+	user_id CHAR,
+	user_name VARCHAR(50),
+	user_stars DECIMAL(3,2),
+  	user_yelp_since DATE,
+  	user_num_of_fans INTEGAR,
+	PRIMARY KEY (user_id) 
+);
+
 CREATE TABLE Friend (
 	friend_name VARCHAR(50),
 	friend_stars DECIMAL(3,2),
 	friend_yelp_since DATE,
 	PRIMARY KEY (friend_name)
-
-CREATE TABLE User (
-	user_ID CHAR,
-	user_name VARCHAR(50),
-	user_stars DECIMAL(3,2),
-  	user_yelp_since DATE,
-  	user_num_of_fans INTEGAR,
-	PRIMARY KEY (user_ID) 
+	FOREIGN KEY (user_id) REFERENCES User(user_id)
 );
+
+CREATE TABLE Votes (
+	votes_funny INTEGER,
+	votes_Cool INTEGER,
+	votes_Useful INTEGER,
+	FOREIGN KEY (user_id) REFERENCES User(user_id)
+	FOREIGN KEY (review_id) REFERENCES Review(review_id)
+);
+
 CREATE TABLE Review(
 	review_author CHAR,
 	review_id INTEGAR,
@@ -19,6 +30,6 @@ CREATE TABLE Review(
 	review_date DATE,
 	review_rating DECIMAL(3,2),
 	PRIMARY KEY(review_id)
-	
 );
+
 
